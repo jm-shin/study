d8aa5c3d1a9f574cb85d4b74db703131f5666a31
jongminshin <ace4463@gmail.com>
2020-03-08 14:39:33 +0900

    HTTP -Get Method study

---
+++ src/main/java/com/example/study/controller/GetController.java
@@ -0,0 +1,39 @@
+package com.example.study.controller;
+
+import com.example.study.model.SearchParam;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api") //localhost:8080/api
+public class GetController {
+
+    @RequestMapping(method = RequestMethod.GET, path = "/getMethod") //localhost:8080/api/getMethod
+    public String getRequest(){
+
+        return  "Hi getMethod";
+    }
+
+    @GetMapping("/getParameter") //localhost:8080/api/getParameter?id=1234&password=abcd
+    public String getParameter(@RequestParam String id, @RequestParam(name = "password") String pwd){
+        String password = "bbb";
+
+        System.out.println("id:"+id);
+        System.out.println("password"+pwd);
+        return id+password;
+
+    }
+
+    //localhost:8080/api/multiParameter?account=abcd&mail=study@gmail.com
+    @GetMapping("/getMultiParameter")
+    public SearchParam getMultiParameter(SearchParam searchParam){
+
+        System.out.println(searchParam.getAccount());
+        System.out.println(searchParam.getEmail());
+        System.out.println(searchParam.getPage());
+
+        //{"acount":"", "mail":"", "page":0}
+        return searchParam;
+    }
+
+
+}
